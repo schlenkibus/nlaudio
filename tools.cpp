e495d08e445ceb8d35e2500429a4b7578d2b07af
Pascal Huerst <pascal.huerst@gmail.com>
2016-01-25 16:31:32 +0100

    sinewave: Fix generation functions for float and double

--- tools.cpp
+++ tools.cpp
@@ -3,6 +3,36 @@
 
 namespace Nl {
 
+#include <sched.h>
+
+/** \ingroup Tools
+ *
+ * \brief Request and set realtime priority for this process
+ * \return 0 on success, an negative errorcode otherwise
+ *
+ * Request and set realtime priority for this process. Also scheduler
+ * policy is changed to round robin
+ *
+*/
+int requestRealtime(void)
+{
+		struct sched_param sched_param;
+		int ret = sched_getparam(0, &sched_param);
+		if (ret != 0) {
+				printf("Scheduler getparam failed...\n");
+				return ret;
+		}
+		sched_param.sched_priority = sched_get_priority_max(SCHED_RR);
+		ret = sched_setscheduler(0, SCHED_RR, &sched_param);
+		if (ret != 0) {
+				printf("Scheduler setprio failed...\n");
+				return ret;
+		}
+
+		printf("!!! Scheduler set to Round Robin with priority %i !!!\n", sched_param.sched_priority);
+		return 0;
+}
+
 /*
 auto getTypeForAudioSpec(const SampleSpecs_t &specs)
 {
